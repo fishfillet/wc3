2c2d56f593a46914c49b2c8fa2e83c781d79f53f
fishfillet <daniel.susetyo@gmail.com>
2015-03-11 02:10:36 +0000

    add enhancement 2

--- spec/test_12.rb
+++ spec/test_12.rb
@@ -1,2 +1,21 @@
 require_relative 'spec_helper'
 
+describe Unit do
+  
+  before :each do 
+    @unit = Unit.new(0, 12)
+  end
+
+  # Units start off alive but they die if their HP hits 0 or lower. 
+  # This is usually a result of receiving damage from combat.
+
+  describe "#dead?" do
+    it "returns true if unit runs out of HP" do 
+      expect(@unit.dead?).to be_truthy
+    end
+  end
+
+
+
+
+end  
